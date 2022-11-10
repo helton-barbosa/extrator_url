178cc2f00be14b049132e2d3d2e84114d7262255
Helton Barbosa <heltonbs@gmail.com>
2022-11-10 12:11:37 -0300

    find()
    utilizando o método find para separar dinamicamente a URL

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "https://bytebank.com/cambio?moedaOrigem=real"
+print(f'URL completa: {url}')
 
-url_base = url[0:19]
-print(url_base)
+indice_interrogacao = url.find('?')
 
-url_parametros = url[20:36]
-print(url_parametros)
+url_base = url[:indice_interrogacao]
+print(f'URB base: {url_base}')
+
+url_parametros = url[indice_interrogacao+1:]
+print(f'URL parâmetros: {url_parametros}')
