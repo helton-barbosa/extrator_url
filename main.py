73bc4b682fde5bc275e100723b8413760068b002
Helton Barbosa <heltonbs@gmail.com>
2022-11-10 12:26:35 -0300

    len()
    usando o método len()

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
-print(f'URL completa: {url}')
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+# print(f'URL completa: {url}')
 
 indice_interrogacao = url.find('?')
 
 url_base = url[:indice_interrogacao]
-print(f'URB base: {url_base}')
+# print(f'URB base: {url_base}')
 
 url_parametros = url[indice_interrogacao+1:]
 print(f'URL parâmetros: {url_parametros}')
+
+parametro_de_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_de_busca)
+indice_valor = indice_parametro + len(parametro_de_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
