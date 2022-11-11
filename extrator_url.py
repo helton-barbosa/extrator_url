88ccd21722204203a063193b9b2342c5cfa01667
Helton Barbosa <heltonbs@gmail.com>
2022-11-11 15:12:01 -0300

    método len()

--- extrator_url.py
+++ extrator_url.py
@@ -41,7 +41,11 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
 
-extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real")
+url = "bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
+extrator_url = ExtratorURL(url)
+print("O tamanho da URL: ", len(extrator_url))
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
