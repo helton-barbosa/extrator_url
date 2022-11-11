4bfe4a9348129c5abe735b788203fadfc8e7ddc4
Helton Barbosa <heltonbs@gmail.com>
2022-11-11 10:55:51 -0300

    expressões regulares

---
+++ extrator_cep.py
@@ -0,0 +1,9 @@
+import re
+
+endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 77060686"
+
+padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+busca = padrao.search(endereco)
+if busca:
+    cep = busca.group()
+    print(cep)
