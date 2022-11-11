39307f564b432885b15a229423451fa448d08577
Helton Barbosa <heltonbs@gmail.com>
2022-11-11 11:03:00 -0300

    usando qualificadores e intervalos

--- extrator_cep.py
+++ extrator_cep.py
@@ -2,7 +2,7 @@ import re
 
 endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 77060686"
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{2}[.]?[0-9]{3}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco)
 if busca:
     cep = busca.group()
